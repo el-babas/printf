514dda61476c697bfcc2f340779a6ee271d1df1b
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-08 13:45:28 -0500

    Add file write_fill.c

--- write_fill.c
+++ write_fill.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * fill_malloc - fill buffer reserved
+ * @str_malloc: buffer string
+ * @character: letter
+ * @pos: position
+ *
+ * Return: position buffer
+ */
 unsigned int fill_malloc(char *str_malloc, char character, unsigned int pos)
 {
 	*(str_malloc + pos) = character;
@@ -7,6 +15,13 @@ unsigned int fill_malloc(char *str_malloc, char character, unsigned int pos)
 	return (pos);
 }
 
+/**
+ * write_malloc - write buffer reserved
+ * @str_malloc: buffer string
+ * @byte_malloc: size bytes
+ *
+ * Return: write
+ */
 unsigned int write_malloc(char *str_malloc, unsigned int byte_malloc)
 {
 	return (write(1, str_malloc, byte_malloc));
