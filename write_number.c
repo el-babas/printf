ed313b2717bf0b9020be29e1e78e10770594fdf2
cristhian1107 <cristhian.cjaa@gmail.com>
2021-11-08 13:57:41 -0500

    Add file write_number.c

---
+++ write_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * write_int - print %i and %d
+ * @args: string ...
+ * @str_malloc: buffer reserved
+ * @pos: position
+ *
+ * Return: length write
+ */
+int write_int (va_list args, char *str_malloc, unsigned int pos)
+{
+	int number = va_arg(args, int);
+	int len = 0;
+
+	/* if number < 0, convert to positive and push - to buffer */
+	if (number < 0)
+	{
+		number *= -1;
+		pos = fill_malloc(str_malloc, '-', pos);
+	}
+	len = convert_base(10, number, str_malloc, pos, 0);
+	/* validar y pos - 1 = '\0' */
+	if (len == 0)
+	{
+		pos = fill_malloc(str_malloc, '\0', pos - 1);
+	}
+	return (len);
+}
+
+/**
+ * write_unint - print %u
+ * @args: string ...
+ * @str_malloc: buffer reserved
+ * @pos: position
+ *
+ * Return: length write
+ */
+int write_unint (va_list args, char *str_malloc, unsigned int pos)
+{
+	unsigned long int number = va_arg(args, unsigned long int);
+	int len = 0;
+
+	len = convert_base(10, number, str_malloc, pos, 0);
+	return (len);
+}
