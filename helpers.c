80c995156641fed72fe6cf7f09a6a20e804e0616
Carlos Bentin <3669@holbertonschool.com>
2021-11-09 16:44:25 +0000

    adding helper functions

---
+++ helpers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: integer n to print using _putchar
+ *
+ * Return: none - void function
+ *
+ *
+ */
+int print_number(int n)
+{
+	unsigned int r;
+	int _length = 0;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		n *= -1;
+	}
+	r = n;
+
+	if (r / 10)
+	{
+		_length += print_number(r / 10);
+	}
+	_putchar(r % 10 + '0');
+	_length++;
+	return (_length++);
+}
