8740b37c4f99b024b93403e8e897aa956705e5d5
Carlos Bentin <cbentin12@gmail.com>
2021-11-13 14:02:40 -0500

    Adding TEST cases

--- Tests/tests.c
+++ Tests/tests.c
@@ -6,6 +6,10 @@ int main(int argc, char **argv)
 	long int l = INT_MIN;
 	long int lm = UINT_MAX;
 	long res = INT_MAX;
+	/*char *str = "When %s invoke GCC, it %s and linking. The %coverall options%c allow you to stop this process at an intermediate stage. %s the -c option says not to run the linker. %s preprocessor and %cthers the compiler itself. Yet other %s need to use any of them.\n\nMost of the command line options that you can use with GCC are useful for C programs; when an option is only useful with another language (usually C ++ ), the explanation says so explicitly. If the description for a particular option does not mention a source language, you can use that option with all supported languages.\n\nThe gcc program accepts options and file names as operands. Many options have multi-letter names; therefore multiple single-letter options may not%d\n";
+	char *tmp = "Then the output consists of object files output by the assembler.\n\nOther options are passed on to one stage of processing. Some options control the";
+	char *tmp2 = "options control the assembler and linker; most of these are not documented here, since you rarely";*/
+		
 
 	int TASK_NUM = 0;
 
@@ -13,6 +17,9 @@ int main(int argc, char **argv)
 	(void)lm;
 	(void)res;
 	(void)argc;
+	/*(void)str;
+	(void)tmp;
+	(void)tmp2;*/
 
 	if (argc > 2)
 	{
@@ -515,9 +522,7 @@ int main(int argc, char **argv)
 		len2 = printf("\t--> Hello, %s. project took us %+d days, which in hexa is %#x\n", "World", 5, 5); printf("\t--> Lenth of string: %d\n\n", len2);
 		printf("--------------------------------------------------------------------------\n");
 		printf("\nResult from our _printf: \n\n");	
-		len = _printf("\t--> tHello, %s. project took us %+d days, which in hexa is %#x\n", "World", 5, 5); printf("\t--> Length of string: %d\n\n\n", len);
-		
-		_printf("%#x", 250);
+		len = _printf("\t--> Hello, %s. project took us %+d days, which in hexa is %#x\n", "World", 5, 5); printf("\t--> Length of string: %d\n\n\n", len);
 		break;
 
 	case 11:
@@ -530,6 +535,26 @@ int main(int argc, char **argv)
 		_printf("%x\n", 250);
 		_printf("%X\n", 250);
 		_printf("%b\n", 250);
+		break;
+
+	case 5:
+		/*printf("TEST CASES FOR TASK %d\n======================\n======================\n\n", TASK_NUM);	
+		printf("PRUEBA 2  ============================\n");
+		 char *str = "When you invoke GCC, it normally does preprocessing, compilation, assembly and linking. The \"overall options\" allow you to stop this process at an intermediate stage. For example, the -c option says not to run the linker. Then the output consists of object files output by the assembler.\n\nOther options are passed on to one stage of processing. Some options control the preprocessor and others the compiler itself. Yet other options control the assembler and linker; most of these are not documented here, since you rarely need to use any of them.\n\nMost of the command line options that you can use with GCC are useful for C programs; when an option is only useful with another language (usually C ++ ), the explanation says so explicitly. If the description for a particular option does not mention a source language, you can use that option with all supported languages.\n\nThe gcc program accepts options and file names as operands. Many options have multi-letter names; therefore multiple single-letter options may not%d\n";
+		len2 = printf(str, 123456789); printf("\n%d\n", len2); 
+		len = _printf(str, 123456789); printf("\n%d\n", len);
+		
+		printf("TEST CASES FOR TASK %d\n======================\n======================\n\n", TASK_NUM);	
+		printf("PRUEBA 3  ============================\n");
+		len2 = printf(str, "you", "normally does preprocessing, compilation, assembly", '"', '"', "For example,", tmp, 'o', tmp2, 8); printf("\n%d\n", len2);
+		len = _printf(str, "you", "normally does preprocessing, compilation, assembly", '"', '"', "For example,", tmp, 'o', tmp2, 8); printf("\n%d\n", len);
+		fflush(stdout);
+
+		printf("TEST CASES FOR TASK %d\n======================\n======================\n\n", TASK_NUM);	
+		printf("PRUEBA 4  ============================\n");
+		len = _printf(str, "you", "normally does preprocessing, compilation, assembly", '"', '"', "For example,", tmp, 'o', tmp2, 123456789);
+		len2 = printf(str, "you", "normally does preprocessing, compilation, assembly", '"', '"', "For example,", tmp, 'o', tmp2, 123456789);
+		*/
 		break;
 	}
 		
